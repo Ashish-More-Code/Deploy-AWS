74ef8231c64c8b0a8ee7653847f4caeb42c35114
ashish <ashishbmore1@gmail.com>
2025-11-17 19:29:42 +0530

    updated response

--- myproject/api/views.py
+++ myproject/api/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from django.http import HttpResponse
+ 
 def hello_world(request):
-    return JsonResponse({"message": "Hello"})
+    message = "ashish"
+    html = f"""
+    <div style="font-family: Arial; padding: 20px;">
+        <h1 style="color: green;">{message}</h1>
+    </div>
+    """
+    return HttpResponse(html, content_type="text/html")
